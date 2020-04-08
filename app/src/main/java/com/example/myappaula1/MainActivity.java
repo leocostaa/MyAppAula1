005abf6399525e6175dfc23b81675f8622cc49c1
leocostaa <32634126+leocostaa@users.noreply.github.com>
2020-04-08 10:07:17 +0100

    Alteração

--- app/src/main/java/com/example/myappaula1/MainActivity.java
+++ app/src/main/java/com/example/myappaula1/MainActivity.java
@@ -19,26 +19,27 @@ public class MainActivity extends AppCompatActivity {
         // Do something in response to button
         Intent intent = new Intent(this, DisplayMessageActivity.class);
 
-        EditText editText2 = (EditText) findViewById(R.id.editText2);
-        EditText editText3 = (EditText) findViewById(R.id.editText3);
-        EditText editText4 = (EditText) findViewById(R.id.editText4);
-        EditText editText5 = (EditText) findViewById(R.id.editText5);
-        EditText editText6 = (EditText) findViewById(R.id.editText6);
-        EditText editText7 = (EditText) findViewById(R.id.editText7);
+        EditText editNome = (EditText) findViewById(R.id.editText2);
+        EditText editAge = (EditText) findViewById(R.id.editText3);
+        EditText editEmail = (EditText) findViewById(R.id.editText4);
+        EditText editHeight = (EditText) findViewById(R.id.editText5);
+        EditText editPhone = (EditText) findViewById(R.id.editText6);
+        EditText editWeight = (EditText) findViewById(R.id.editText7);
 
-        String message2 = editText2.getText().toString();
-        String message3 = editText3.getText().toString();
-        String message4 = editText4.getText().toString();
-        String message5 = editText5.getText().toString();
-        String message6 = editText6.getText().toString();
-        String message7 = editText7.getText().toString();
 
-        intent.putExtra("Nome", message2);
-        intent.putExtra("Age", message3);
-        intent.putExtra("Email", message4);
-        intent.putExtra("Height", message5);
-        intent.putExtra("PhoneNumber", message6);
-        intent.putExtra("Weight", message7);
+        String Nome = editNome.getText().toString();
+        String Age = editAge.getText().toString();
+        String Email = editEmail.getText().toString();
+        String Height = editHeight.getText().toString();
+        String Phone = editPhone.getText().toString();
+        String Weight = editWeight.getText().toString();
+
+        intent.putExtra("Nome", Nome);
+        intent.putExtra("Age", Age);
+        intent.putExtra("Email", Email);
+        intent.putExtra("Height", Height);
+        intent.putExtra("PhoneNumber", Phone);
+        intent.putExtra("Weight", Weight);
 
         startActivity(intent);
     }
